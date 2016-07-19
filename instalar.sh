2b8f0c16fce88079a853459d34a0cf822da801dd
Miguel Lopes <miguellopes@blackbox.lan>
2016-07-19 21:25:06 +0100

    new installation script

--- instalar.sh
+++ instalar.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
+SHH=`echo $SHELL | cut -d"/" -f3`
+SHL="zsh"
 if [ -z "$1" ]
 then
+   ls README.md > /dev/null 2>&1
+   if [ "$?" != "0" ]; then
+      echo "A extrair repositorio."
+      git checkout .
+   fi
+   ls zsh/.oh-my-zsh/oh-my-zsh.sh > /dev/null 2>&1
+   if [ "$?" != "0" ]; then
+      echo "A atualizar submodulos."
+      git submodule init zsh/.oh-my-zsh/
+      git submodule update zsh/.oh-my-zsh/
+   fi
 	find . -maxdepth 2 -name ".*" ! -name '.git*' ! -name '.' ! -name '..' -exec ln -s -f $PWD/{} --target-directory=$HOME \;
 else
-	scp -r $PWD $1:~/
-	echo git remote add NOME ssh://MAQUINA/home/USER/.dotfiles/git
+   rsync -avzr $PWD/.git $1:~/.dotfiles/
+   rsync -avzr $PWD/$0 $1:~/.dotfiles/
 fi
 
