762176295d84c3cf43e6a9219a0b901190fc1a45
Miguel Lopes <miguellopes@blackbox.lan>
2016-07-19 15:43:43 +0100

    left behind

--- instalar.sh
+++ instalar.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 if [ -z "$1" ]
-  then
-      find ~/.dotfiles -maxdepth 1 -name ".*" ! -name '.git' ! -name '.' ! -name '..' -exec ln -s -f {} --target-directory=$HOME \;
-   else
-      scp -r $PWD $1:~/
-      echo git remote add NOME ssh://MAQUINA/home/USER/.dotfiles/git
-   fi
+then
+	find . -maxdepth 2 -name ".*" ! -name '.git*' ! -name '.' ! -name '..' -exec ln -s -f $PWD/{} --target-directory=$HOME \;
+else
+	scp -r $PWD $1:~/
+	echo git remote add NOME ssh://MAQUINA/home/USER/.dotfiles/git
+fi
 
