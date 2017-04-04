51469059b5c6db0642dd2d53e1809916b00a9c36
Miguel Lopes <miguellopes@localhost.localdomain>
2017-04-04 05:06:35 +0100

    bart updates

---
+++ bin/tiling.py
@@ -0,0 +1,54 @@
+#! /usr/bin/python2.7
+
+# tiling.py
+# Simple tiling for GTK
+
+from gtk.gdk import *
+import argparse
+
+TITLE_BAR = 22  # Put the height of your title bar here
+
+# Parse the command line arguments
+
+parser = argparse.ArgumentParser(description="Simple tiling for GTK")
+parser.add_argument("w", type=int,
+        help="Frame width (in percentage of screen width)")
+parser.add_argument("h", type=int,
+        help="Frame height (in percentage of screen height)")
+parser.add_argument("x", type=int,
+        help="Frame x coordinate (in percentage of screen width)")
+parser.add_argument("y", type=int,
+        help="Frame y coordinate (in percentage of screen height)")
+parser.add_argument("-d", type=int, choices=[0, 1], default=1,
+        help="1 for window decorations, 0 for none")
+args = parser.parse_args()
+
+win = window_foreign_new((get_default_root_window()
+    .property_get('_NET_ACTIVE_WINDOW')[2][0]))
+state = win.property_get('_NET_WM_STATE')[2]
+
+# Get the screen's width and height
+
+screen_width = screen_width()
+screen_height = screen_height()
+
+# Calculate the frame dimensions and location in pixels
+
+w = int(round(args.w / 100.0 * screen_width))
+h = int(round(args.h / 100.0 * screen_height))
+x = int(round(args.x / 100.0 * screen_width))
+y = int(round(args.y / 100.0 * screen_height))
+
+# Check whether decorations are desired
+
+if args.d == 1:
+    win.set_decorations(DECOR_TITLE)  # or DECOR_ALL for all decorations
+    win.move_resize(x, y, w, (h - TITLE_BAR))
+if args.d == 0:
+    win.set_decorations(0)
+    win.move_resize(x, y, w, h)
+
+    # Apply changes to window
+
+window_process_all_updates()
+flush()
---
+++ bin/workon.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#incia ambiente webdev
+wmctrl -s 1
+exec chromium &
+exec urxvt &
+sleep 3
+ST=`wmctrl -l |grep Chromium | cut -d" " -f1`
+ter=`wmctrl -l |grep "1 bart miguellopes"| cut -d" " -f1`
+wmctrl -ir $ST -e 1,0,24,1200,1416
+wmctrl -ir $ter -e 1,1200,24,2240,1390
